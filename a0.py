a0599280d87148dcb958b871b9278f443331e2ae
Rohit Rairchan <rraricha@uci.edu>
2024-01-15 23:07:18 -0800

    Writing the main function

---
+++ a0.py
@@ -0,0 +1,18 @@
+# a0.py
+
+# Starter code for assignment 0 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Rohit George Rarichan
+# rraricha@uci.edu
+# 36126645
+def main():
+    n = int(input())
+    s = " "
+    start = "+"
+    if n > 0 and n < 1000:
+        for i in range(0, n):
+            start+= output(i,s)
+
+
